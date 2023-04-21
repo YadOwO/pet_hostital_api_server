76f91b78bc3d89fcb98f3b57ad76687fbd0463d4
yado <1340324421@qq.com>
2023-04-22 00:03:16 +0800

    add 【更新医生头像】

--- router/doctorManage.js
+++ router/doctorManage.js
@@ -7,13 +7,17 @@ const doctorManageHandler = require('../router_handler/doctorManage')
 // 1. 导入验证表单数据的中间件
 const expressJoi = require('@escook/express-joi')
 // 2. 导入需要的验证规则对象
-const { add_doctor_schema, delete_doctor_schema, update_doctor_schema } = require('../schema/doctor')
+const { add_doctor_schema, delete_doctor_schema, 
+        update_doctor_schema, update_avatar_schema } = require('../schema/doctor')
 
 // 新增医生
 router.post('/addDoctor', expressJoi(add_doctor_schema), doctorManageHandler.addDoctor)
 
 // 更新医生信息
 router.post('/updateDoctor', expressJoi(update_doctor_schema), doctorManageHandler.updateDoctor)
+
+// 更新医生头像
+router.post('/updateAvatar', expressJoi(update_avatar_schema), doctorManageHandler.updateAvatar)
 
 // 获取所有医生信息
 router.get('/getAllDoctor', doctorManageHandler.getAllDoctor)
